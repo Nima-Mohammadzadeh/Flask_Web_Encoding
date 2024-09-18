51d53ef535051eb400a57e6580bef54e4397a32f
Nima Zadeh <97640172+Nima-Mohammadzadeh@users.noreply.github.com>
2024-09-18 16:46:26 +0000

    database class created for serial number

--- app/models.py
+++ app/models.py
@@ -3,5 +3,11 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
 
-##class serialNumber(db.model):
+class serialNumber(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    CurrentSerial: so.Mapped[int] = so.mapped_column(sa.Integer, default=0)
+
+
+    def __repr__(self):
+        return '<Serial: {}>'.format(self.CurrentSerial)
     
