b335dadc01ce01a71eb36be4cbb2ed1b71002113
Nima Zadeh <97640172+Nima-Mohammadzadeh@users.noreply.github.com>
2024-09-18 17:45:21 +0000

    created database for use. one record starting at 1000 integer

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import DatabaseForm
+from app.models import serialNumber
+from sqlalchemy import select
+from app import db
 
 @app.route('/')
 @app.route('/index')
@@ -10,9 +13,19 @@ def Home():
 
 @app.route('/job-setup', methods=['GET', 'POST'])
 def job_setup():
+
     form = DatabaseForm()
+
+    query = select(serialNumber.CurrentSerial)
+    query_result = db.session.execute(query)
+    serial = query_result.scalar_one_or_none()
+
+
+   
+
     if form.validate_on_submit():
         flash('Databse Requested')
+
 
         return redirect(url_for('Home'))
 
