9001277729eb2deaa839f4f023054d9381cf1289
Nima Zadeh <97640172+Nima-Mohammadzadeh@users.noreply.github.com>
2024-09-18 19:35:32 +0000

    UPC validation and serial database linkde correctly

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired
+from wtforms.validators import ValidationError, DataRequired, Length
 
 class DatabaseForm(FlaskForm):
-    serial = IntegerField('Serial Number', validators=[DataRequired()])
-    UPC = IntegerField('UPC', validators=[DataRequired()])
+    serial = IntegerField('Serial Number', render_kw={'readonly': True})
+    UPC = StringField('UPC', validators=[
+        DataRequired(),
+        Length(min=12, max=12, message="UPC must be exactly 12 characters long")
+    ])
     quantity = IntegerField('Quantity', validators=[DataRequired()])
     two_percent = BooleanField('2%')
     seven_percent = BooleanField('7%')
     submit = SubmitField('Generate')
+
+    def validate_UPC(self, field):
+        if not field.data.isdigit():
+            raise ValidationError("UPC must contain only digits")
--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import DatabaseForm
 from app.models import serialNumber
-from sqlalchemy import select
+from sqlalchemy import select, update
 from app import db
 
 @app.route('/')
@@ -16,18 +16,19 @@ def job_setup():
 
     form = DatabaseForm()
 
-    query = select(serialNumber.CurrentSerial)
-    query_result = db.session.execute(query)
-    serial = query_result.scalar_one_or_none()
+    current_serial = db.session.scalar(select(serialNumber.CurrentSerial))
+    form.serial.data = current_serial
 
 
    
 
     if form.validate_on_submit():
         flash('Databse Requested')
+        new_serial = current_serial + form.quantity.data
+        serialNumber.query.update({serialNumber.CurrentSerial: new_serial})
+        db.session.commit()
 
-
-        return redirect(url_for('Home'))
+        return redirect(url_for('job_setup'))
 
     return render_template('job-setup.html', title='Job Setup', form=form )
 
