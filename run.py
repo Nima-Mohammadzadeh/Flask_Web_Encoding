9f6b9729ab599ddaa75d45867632a2abebd3c2a1
Nima Zadeh <97640172+Nima-Mohammadzadeh@users.noreply.github.com>
2024-09-19 15:00:38 +0000

    -

--- run.py
+++ run.py
@@ -3,7 +3,9 @@ import sqlalchemy.orm as so
 from app import app, db
 from app.models import serialNumber
 
-
+if __name__ == '__main__':
+    app.run(debug=True)
+    
 @app.shell_context_processor
 def make_shell_context():
     return {'sa': sa, 'so': so, 'db': db, 'serialNumber': serialNumber}
