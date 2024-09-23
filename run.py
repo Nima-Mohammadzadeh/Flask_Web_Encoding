d4b8d51ca85968e2ce148a0817d9861d86e5955f
Nima Zadeh <97640172+Nima-Mohammadzadeh@users.noreply.github.com>
2024-09-23 16:39:23 +0000

    Live Serial updates across users

--- run.py
+++ run.py
@@ -1,12 +1,11 @@
+from app import app, db, socketio
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import app, db, socketio
 from app.models import serialNumber
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    socketio.run(app, debug=True, host='0.0.0.0' , port=5001)
-    
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+
 @app.shell_context_processor
 def make_shell_context():
     return {'sa': sa, 'so': so, 'db': db, 'serialNumber': serialNumber}
